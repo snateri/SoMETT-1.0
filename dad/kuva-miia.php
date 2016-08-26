7cc59d36317a812267626315893dab40403fbd70
MiiaHuvila <mangamirkku@gmail.com>
2016-08-26 10:36:12 +0300

    navin runko lisattu

--- dad/kuva-miia.php
+++ dad/kuva-miia.php
@@ -5,11 +5,21 @@
 <title>Dropzone simple example</title>
 <script src="foundation.js"></script>
 <script src="https://rawgit.com/enyo/dropzone/master/dist/dropzone.js"></script>
-<link rel="stylesheet" href="foundation.css">
 <link rel="stylesheet" href="https://rawgit.com/enyo/dropzone/master/dist/dropzone.css">
+<link rel="stylesheet" href="foundation.css">
 </head>
 <body>
   <!-- Navipalkki tuohon allapuolelle -->
+<div class="top-bar">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text">SoMETT</li>
+      <li><a href="#">Etusivu</a></li>
+      <li><a href="#">Kuvapankki</a></li>
+      <li><a href="#">Lisäyspalvelu</a></li>
+    </ul>
+  </div>
+</div>
   <!-- Jotain paskaa tuohon alapuolelle -->
   <!-- Kuvien laitto juttu -->
   <form action="upload.php" class="dropzone"></form>
