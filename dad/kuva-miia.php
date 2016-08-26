94463f28753f6b6896013940c842a8f4cd0718a8
MiiaHuvila <mangamirkku@gmail.com>
2016-08-26 09:36:21 +0300

    minun versio

---
+++ dad/kuva-miia.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+
+<meta charset="utf-8">
+
+<title>Dropzone simple example</title>
+
+
+<!--
+  DO NOT SIMPLY COPY THOSE LINES. Download the JS and CSS files from the
+  latest release (https://github.com/enyo/dropzone/releases/latest), and
+  host them yourself!
+-->
+<script src="dropzone.js"></script>
+<link rel="stylesheet" href="dropzone.css">
+
+<!-- Change /upload-target to your upload address -->
+<form action="upload.php" class="dropzone"></form>
+
+
