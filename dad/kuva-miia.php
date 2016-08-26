2f1117b97fdc829116ee8e816fb4632cae26d81e
MiiaHuvila <mangamirkku@gmail.com>
2016-08-26 10:11:12 +0300

    lisaa tavaraa

--- dad/kuva-miia.php
+++ dad/kuva-miia.php
@@ -1,20 +1,16 @@
-
 <!DOCTYPE html>
-
+<html>
+<head>
 <meta charset="utf-8">
-
 <title>Dropzone simple example</title>
-
-
-<!--
-  DO NOT SIMPLY COPY THOSE LINES. Download the JS and CSS files from the
-  latest release (https://github.com/enyo/dropzone/releases/latest), and
-  host them yourself!
--->
-<script src="dropzone.js"></script>
-<link rel="stylesheet" href="dropzone.css">
-
-<!-- Change /upload-target to your upload address -->
-<form action="upload.php" class="dropzone"></form>
-
+<script src="foundation.js"></script>
+<script src="https://rawgit.com/enyo/dropzone/master/dist/dropzone.js"></script>
+<link rel="stylesheet" href="foundation.css">
+<link rel="stylesheet" href="https://rawgit.com/enyo/dropzone/master/dist/dropzone.css">
+</head>
+<body>
+  <!-- Upload Thing -->
+  <form action="upload.php" class="dropzone"></form>
+</body>
+</html>
 
