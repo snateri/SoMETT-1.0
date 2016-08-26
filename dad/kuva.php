c814defe46e4562ea1a0543fd786405ddddae86b
TomiJy <tomi.jylha@student.kpedu.fi>
2016-08-26 09:43:07 +0300

    Update kuva.php

--- dad/kuva.php
+++ dad/kuva.php
@@ -1,5 +1,3 @@
-
-
 <script src="https://rawgit.com/enyo/dropzone/master/dist/dropzone.js"></script>
 <link rel="stylesheet" href="https://rawgit.com/enyo/dropzone/master/dist/dropzone.css">
 <form action="/upload-target" class="dropzone"></form>
