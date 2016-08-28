59aa3be89676cb11c9f5c2631e9b41d7a9de22c7
MiiaHuvila <mangamirkku@gmail.com>
2016-08-28 14:33:05 +0300

    main osion tekoa

--- dad/kuva-miia.php
+++ dad/kuva-miia.php
@@ -2,11 +2,12 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Dropzone simple example</title>
+<title>SoMETT</title>
 <script src="foundation.js"></script>
 <script src="https://rawgit.com/enyo/dropzone/master/dist/dropzone.js"></script>
 <link rel="stylesheet" href="https://rawgit.com/enyo/dropzone/master/dist/dropzone.css">
 <link rel="stylesheet" href="foundation.css">
+<link rel="stylesheet" href="tyylit-miia.css">
 </head>
 <body>
   <!-- Navipalkki tuohon allapuolelle -->
@@ -21,9 +22,15 @@
   </div>
 </div>
   <!-- Jotain paskaa tuohon alapuolelle -->
+<section class="main">
+  <div class="small-11 small-centered columns">
   <!-- Kuvien laitto juttu -->
-  <form action="upload.php" class="dropzone"></form>
+    <form action="upload.php" class="dropzone"></form>
+  </div>
+</section>
   <!-- Footer tuohon alapuolelle -->
+<footer>
+</footer>
 </body>
 </html>
 
