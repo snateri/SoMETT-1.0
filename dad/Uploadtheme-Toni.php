d2ad30d8e0912572582ff9d11b866e9c24a86260
ToniIsosaari <isosaaritoni@gmail.com>
2016-08-31 08:20:50 +0300

    Tyylisivu

---
+++ dad/Uploadtheme-Toni.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>SoMETT</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="foundation.css">
+    <script src="foundation.js"></script>
+    <script src="https://rawgit.com/enyo/dropzone/master/dist/dropzone.js"></script>
+    <link rel="stylesheet" href="https://rawgit.com/enyo/dropzone/master/dist/dropzone.css">
+    <link rel="stylesheet" href="http://cosmo.kpedu.fi/~toniisosaari/SoMETT/SoMETT-1.0/dad/Uploadtheme-Toni.css">
+  </head>
+  <body>
+  <div class="top-bar">
+    <div class="top-bar-title">SoMETT</div>
+    <div>
+      <div class="top-bar-left">
+      <a href="#">Main page</a>
+      <a href="#">Send files</a>
+      <a hfer="#">...</a>
+      </div>
+    </div>
+  </div>
+    
+    </div>
+  <div class="container">
+    <div class="row">
+      <div class="large-12 small-12">
+        <img class="main-image" data-interchange="[http://www.hotel-r.net/im/hotel/ge/old-city-19.jpg, (small)]" src="http://www.hotel-r.net/im/hotel/ge/old-city-19.jpg">
+      </div>
+      <div class="large-12 small-12"><h4 class="text-center">Tiedoston lähetys</h4></div>
+      <div class="Send-file">
+        <form action="upload.php" class="dropzone"></form>
+      </div>
+    </div>
+  </div>
+<div class="footer">
+  <div>
+  <p class="text-center">Footer</p>
+  <p class="text-center">-_-</p>
+  </div>
+<div>
