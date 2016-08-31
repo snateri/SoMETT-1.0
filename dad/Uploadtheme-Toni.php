46b23f73e3671277f4e0decde2dfd4e1ddbe11ed
ToniIsosaari <isosaaritoni@gmail.com>
2016-08-31 11:10:13 +0300

    Upload-Toni

--- dad/Uploadtheme-Toni.php
+++ dad/Uploadtheme-Toni.php
@@ -8,34 +8,54 @@
     <script src="https://rawgit.com/enyo/dropzone/master/dist/dropzone.js"></script>
     <link rel="stylesheet" href="https://rawgit.com/enyo/dropzone/master/dist/dropzone.css">
     <link rel="stylesheet" href="http://cosmo.kpedu.fi/~toniisosaari/SoMETT/SoMETT-1.0/dad/Uploadtheme-Toni.css">
-  </head>
+   </head>
   <body>
   <div class="top-bar">
     <div class="top-bar-title">SoMETT</div>
-    <div>
-      <div class="top-bar-left">
-      <a href="#">Main page</a>
-      <a href="#">Send files</a>
-      <a hfer="#">...</a>
+      <div>
+        <div class="top-bar-left">
+        <a class="link" href="#">Main page</a>
+        <a class="link" href="#">Send files</a>
+        <a class="link" hfer="#">...</a>
       </div>
     </div>
   </div>
-    
-    </div>
   <div class="container">
     <div class="row">
-      <div class="large-12 small-12">
-        <img class="main-image" data-interchange="[http://www.hotel-r.net/im/hotel/ge/old-city-19.jpg, (small)]" src="http://www.hotel-r.net/im/hotel/ge/old-city-19.jpg">
+      <div class="large-12 medium-12 small-12">
+        <img class="main-image" alt="Vanha kaupunki" data-interchange="[http://www.hotel-r.net/im/hotel/ge/old-city-19.jpg, (small)]" src="http://www.hotel-r.net/im/hotel/ge/old-city-19.jpg">
       </div>
-      <div class="large-12 small-12"><h4 class="text-center">Tiedoston lähetys</h4></div>
+      <div class="large-12 medium-12 small-12">
+        <h4 class="text-center">Tiedoston lähetys</h4>
+      </div>
       <div class="Send-file">
         <form action="upload.php" class="dropzone"></form>
       </div>
     </div>
+  </div> 
+<!-- For Foundation Icons, put this in your head -->
+<link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+
+<!-- Footer -->
+<footer class="footer">
+  <div class="row full-width">
+    <div class="small-12 medium-3 large-4 columns">
+      <i class="fi-laptop"></i>
+      <h4>Somet</h4>
+      <p>Facebook</p>
+      <p>Twitter</p>
+      <p>Pinterest</p>
+    </div>
+    <div class="small-6 medium-3 large-2 columns">
+      <p></p>
+    </div>
+    <div class="small-6 medium-3 large-2 columns">
+      <i class="fi-address-book"></i>
+      <h4>Yhteystiedot</h4>
+      <p>Puh.</p>
+      <p>Osoite</p>
+      <p>...</p>
+    </div>
   </div>
-<div class="footer">
-  <div>
-  <p class="text-center">Footer</p>
-  <p class="text-center">-_-</p>
-  </div>
-<div>
+</footer>                                                                                                                                                                                </footer>
+</html>
