664e4d17172e0efa58029368eec8e2737c362cb8
MiiaHuvila <mangamirkku@gmail.com>
2016-09-01 13:54:04 +0300

    Lomakkeen hiomista

--- dad/kuva-miia.php
+++ dad/kuva-miia.php
@@ -33,14 +33,13 @@
     <div class="row">
     <div class="small-9 small-centered colums">
       <form method="GET" action="upload.php" class="dropzone">
-        <label>Otsikko
+        <label class="laapeli-b">Otsikko:</label>
           <input class="input-field-b" type="text" placeholder="Käytäthän mahdollisimman kuvailevaa otsikkoa">
-        </label>
-        <label>Kuvailu
-          <input class="input-field-a" type="text" placeholder="Kirjoita kaikki mitä tiedät kyseisestä objektista">
-        </label>
-     </div>
+        <label class="laapeli-a">Kuvailu:</label>
+          <textarea class="input-field-a" type="text" placeholder="Kirjoita kaikki mitä tiedät kyseisestä objektista"></textarea>
+        <button class="hollow button" href="#">Lähetä</button>
       </form>
+    </div>
   </div>
 </section>
   <!-- Apua uusavuttomille -->
